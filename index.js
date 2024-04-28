d758a0448067caa2843b26f72ca7bcbad1ec6ec2
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2024-04-28 19:49:38 +0600

    get added

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
             res.send(result);
         })
 
+        // app.get('/tourism/:id', async(req, res) => {
+        //     const id = req.params.id;
+        //     const query = {_id: new ObjectId(id)}
+        //     const result = await tourismCollection.findOne(query);
+        //     res.send(result);
+        // })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
