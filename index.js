05b865cb45842ceeee79e729c63c5726f04880fe
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2024-04-28 20:19:07 +0600

    tourism card deleted button added

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/tourism/:id', async (req, res) => {
+            const id = req.params.id;
+      
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
